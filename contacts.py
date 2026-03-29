fcdc89db208cb4b043b640d89f7275105fe155e0
Gregorydederchuk <okcana.dedechuk@gmail.com>
2026-03-29 15:41:13 +0300

    Update logic to dictionary and add delete feature

--- contacts.py
+++ contacts.py
@@ -1,11 +1,28 @@
 contacts_list = []
 
+contacts_dict = {}
+
 while True:
-    action = input("1 - Додати контакт, 2 - Переглянути, 3 - Вийти: ")
+    action = input("\n1 - Додати, 2 - Переглянути, 3 - Видалити, 4 - Вийти: ")
+    
     if action == '1':
-        number = input("Введіть номер телефону: ")
-        contacts_list.append(number)
+        name = input("Введіть ПІБ: ")
+        number = input("Введіть номер: ")
+        contacts_dict[name] = number # Збереження у словник
+        print(f"Контакт {name} додано.")
+        
     elif action == '2':
-        print("Список номерів:", contacts_list)
+        print("Список контактів:")
+        for name, number in contacts_dict.items():
+            print(f"{name}: {number}")
+            
     elif action == '3':
+        name = input("Введіть ПІБ для видалення: ")
+        if name in contacts_dict:
+            del contacts_dict[name] # Видалення зі словника
+            print("Контакт видалено.")
+        else:
+            print("Такого контакту немає.")
+            
+    elif action == '4':
         break
